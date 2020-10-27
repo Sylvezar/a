22db477cf40a5776c8174f6c3384a44240fae2a3
Sylvezar <57994409+Sylvezar@users.noreply.github.com>
2020-10-27 20:06:47 +0100

    Update loader.lua

--- autoexe/loader.lua
+++ autoexe/loader.lua
@@ -3,19 +3,18 @@ print("Loading Libraries...")
 local librarylist = {}
 librarylist.gui = "https://raw.githubusercontent.com/Sylvezar/a/main/libraries/gui.lua"
 librarylist.httpget = "https://raw.githubusercontent.com/Sylvezar/a/main/libraries/httpget.lua"
-
-
-print("debug")
-local libraries = {}
+local librariesabc = {}
 local function httpget(url)
 return game:HttpGet(url,true)
 end
 local function include(libraryname,library)
-libraries[libraryname] = library
+librariesabc[libraryname] = library
 end
 for libraryname,library in pairs(librarylist) do
-	print(libraryname,library)
-	include(libraryname,loadstring(httpget(library))())
+	spawn(function()
+		print(libraryname,library)
+		include(libraryname,loadstring(httpget(library))())
+	end)
 end
-_G.libraries = libraries
-return libraries
+_G.framz = librariesabc
+return librariesabc
