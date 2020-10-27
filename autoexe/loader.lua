4f082f9bde6ca2b381ebb8d7486acf768613b257
Sylvezar <57994409+Sylvezar@users.noreply.github.com>
2020-10-27 20:16:25 +0100

    changes

--- autoexe/loader.lua
+++ autoexe/loader.lua
@@ -5,7 +5,20 @@ librarylist.gui = "https://raw.githubusercontent.com/Sylvezar/a/main/libraries/g
 librarylist.httpget = "https://raw.githubusercontent.com/Sylvezar/a/main/libraries/httpget.lua"
 local librariesabc = {}
 local function httpget(url)
-return game:HttpGet(url,true)
+local charset = {}
+for i = 48,  57 do table.insert(charset, string.char(i)) end
+for i = 65,  90 do table.insert(charset, string.char(i)) end
+for i = 97, 122 do table.insert(charset, string.char(i)) end
+function random(length)
+  math.randomseed(os.time())
+
+  if length > 0 then
+    return random(length - 1) .. charset[math.random(1, #charset)]
+  else
+    return ""
+  end
+end
+return game:HttpGet(url..?random(10).."="..random(10),true)
 end
 local function include(libraryname,library)
 librariesabc[libraryname] = library
--- libraries/httpget.lua
+++ libraries/httpget.lua
@@ -1,5 +1,19 @@
 print("Loading HTTPGET libary by Sylvezar")
+local charset = {}
+for i = 48,  57 do table.insert(charset, string.char(i)) end
+for i = 65,  90 do table.insert(charset, string.char(i)) end
+for i = 97, 122 do table.insert(charset, string.char(i)) end
+function random(length)
+  math.randomseed(os.time())
 
-return function(url)
-return game:HttpGet(url,true)
+  if length > 0 then
+    return random(length - 1) .. charset[math.random(1, #charset)]
+  else
+    return ""
+  end
 end
+local function httpget(url)
+return game:HttpGet(url..?random(10).."="..random(10),true)
+end
+
+return httpget
